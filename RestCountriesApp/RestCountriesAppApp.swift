05aa951ce60463301a88b39cec914f78d02f9330
Ahmed Ezzat <Ahmed.ezzat@mdi.com.eg>
2025-09-18 17:17:23 +0300

    first commit

---
+++ RestCountriesApp/RestCountriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestCountriesAppApp.swift
+//  RestCountriesApp
+//
+//  Created by Ahmed Ezzat on 18/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RestCountriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
